542ba0c7f70fc3e7d193e193b25c5115c176d08b
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-12 04:21:21 +0900

    Update

--- insertMany.js
+++ insertMany.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 // 下のURIをご自分の値に変更してください
-const uri = "mongodb+srv://koh:kohtaroh01xxx@cluster0.y5uxhta.mongodb.net/food?retryWrites=true&w=majority";
+const uri = "mongodb+srv://2401335031pc:H8yGCaid7iZ5QDIC@cluster0.73td3cp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 async function run() {
 const database = client.db('notes');
