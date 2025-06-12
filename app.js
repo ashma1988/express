c479da07b2be41a4d90ca1f839b025c1ae054b56
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-12 23:41:27 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var createError = require('http-errors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/cat');
+var catRouter = require('./routes/cat'); // Ensure this file exists
 var dogRouter = require('./routes/dog');
 
 var app = express();
@@ -26,16 +26,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
-app.use('/cat', catRouter);
+app.use('/cat', catRouter); // Ensure this is correctly registered
 app.use('/dog', dogRouter);
 
 // Catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // Error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
