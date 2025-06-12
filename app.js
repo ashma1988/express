12a20422ce068578fdb04f544d22d76795bd8e44
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-12 23:31:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
-
-app.get('/cat', (req, res) => {
-  res.send('Here is your data');
-});
-
 app.listen(3000, () => console.log('Server running on port 30180'));
 
 var createError = require('http-errors');
