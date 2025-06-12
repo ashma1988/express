c94df92b2448d7496912f8cbf8b48be5d0248eaf
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-12 23:53:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
+console.log("Server script started");
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => console.log('Server running on port 30180'));
+
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var createError = require('http-errors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var notesRouter = require('./routes/notes');
-var catRouter = require('./routes/cat'); // Ensure this file exists
+var catRouter = require('./routes/cat');
 var dogRouter = require('./routes/dog');
 
 var app = express();
 
-// View engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,26 +29,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register routes BEFORE error handling
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
-app.use('/cat', catRouter); // Ensure this is correctly registered
+app.use('/cat', catRouter);
 app.use('/dog', dogRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(3000, () => console.log('Server running on port 30180'));
 
 module.exports = app;
