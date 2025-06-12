97be7ecdd4fe46e96c8608eccde433c3d0c7e4b5
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-12 20:07:50 +0900

    Update cat.js

--- routes/cat.js
+++ routes/cat.js
@@ -4,7 +4,7 @@ var fetch = require('node-fetch'); // Using fetch instead of request (request is
 
 router.get('/', async (req, res) => {
   try {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search');
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions');
     if (!response.ok) {
       throw new Error('Failed to fetch cat image');
     }
