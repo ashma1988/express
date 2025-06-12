f37571a272d28125fd2a70f6e4f2dc14997d3967
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-12 19:36:14 +0900

    Update cat.js

--- routes/cat.js
+++ routes/cat.js
@@ -1,13 +1,16 @@
-var express= require('express');
-var router= express.Router();
-const request= require('request');
+var express = require('express');
+var router = express.Router();
+var request = require('request');
 
-router.get('/', async(req, res) => {
-request('https://api.thecatapi.com/v1/images/search', function(error, response, body) {
-if(!error && response.statusCode == 200) {
-const data= JSON.parse(body);
-res.json(data);
-}
+router.get('/', function (req, res) {
+  request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var data = JSON.parse(body);
+      res.json(data);
+    } else {
+      res.status(500).send('Failed to fetch cat image');
+    }
+  });
 });
-})
+
 module.exports = router;
