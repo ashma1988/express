e923614c4ea1bdf8fd8f909a00bfb890a556af14
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-06-15 17:44:48 +0900

    Update dog.js

--- routes/dog.js
+++ routes/dog.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 const request = require('request');
 
-router.get('/', async(req, res) => {
-request('https://images.dog.ceo/breeds/borzoi/n02090622_8814.jpg', function(error, response, body) {
-if(!error && response.statusCode == 200) {
-const data = JSON.parse(body);
-res.json(data);
-}
+router.get('/', async (req, res) => {
+  request('https://images.dog.ceo/breeds/borzoi/n02090622_8814.jpg', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(`<img src="${response.request.uri.href}" alt="Dog Image">`);
+    }
+  });
 });
-})
+
 module.exports = router;
