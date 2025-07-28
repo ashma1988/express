ed514401ee410df610d06f2a086b432fbc0cc58b
ashma1988 <2401335031pc@cyber-u.ac.jp>
2025-07-29 08:02:00 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var router = express.Router();
-// レスポンスのデータ（ノート全件）
+const cors = require('cors'); // corsミドルウェアを追加
+require('dotenv').config();
+
+
+// 接続情報を設定
 const { MongoClient } = require("mongodb");
-// 下のURIをご自分の値に変更してください
-const uri = "mongodb+srv://2401335031pc:H8yGCaid7iZ5QDIC@cluster0.73td3cp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-router.get('/', async(req, res) => {
+// corsミドルウェアを使用
+router.use(cors());
+
+router.get('/', async (req, res) => {
+// データベース、コレクションを指定
 const database = client.db('notes');
 const notes = database.collection('notes');
+
 // idが１のドキュメントを取得
 const query = { id: 2 };
 const note = await notes.findOne(query);
 
 res.json(note);
 })
+
 module.exports = router;
